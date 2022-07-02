1a4f834b5759195084f375c262ef3d764d8836ea
thomasPVF <108387696+thomasPVF@users.noreply.github.com>
2022-07-02 04:04:30 -0400

    portafolio
    nuevo portafolio laboral

---
+++ portafolio/index.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Title</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.0-beta1 -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css"  integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+  </head>
+  <body>
+  <?php include("header.html");?>
+    <div class="container-xxl"><br>
+        <h1 class="titulo" style="color: white;">Nuestros desarrollos!!</h1><br>
+
+
+        <div class="row">
+            <div class="col-6">
+                <div class="card text-start">
+                  <img class="card-img-top" src="marcela.png" alt="Title">
+                  <div class="card-body">
+                    <h4 class="card-title">Marcelas Pasteleria</h4>
+                    <p class="card-text">Pasteleria chilena, LA MEJOR</p>
+                  </div>
+                </div>
+            </div>
+            <div class="col-6">
+               <div class="card text-start">
+                  <img class="card-img-top" src="marcela.png" alt="Title">
+                  <div class="card-body">
+                    <h4 class="card-title">Marcelas Pasteleria</h4>
+                    <p class="card-text">Pasteleria chilena, LA MEJOR</p>
+                  </div>
+                </div>
+            </div>
+        </div><br>
+        
+
+
+    </div><br><br><br><br>
+    
+    <?php include("footer.html");?>
+      
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.5/dist/umd/popper.min.js" integrity="sha384-Xe+8cL9oJa6tN/veChSP7q+mnSPaj5Bcu9mPX5F5xIGE0DVittaqT5lorf0EI7Vk" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.min.js" integrity="sha384-kjU+l4N0Yf4ZOJErLsIcvOU2qSb74wXpOhqTvwVx3OElZRweTnQ6d31fXEoRD1Jy" crossorigin="anonymous"></script>
+  </body>
+</html>
